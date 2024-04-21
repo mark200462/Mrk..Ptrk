a9ea0e050343dce585f92273cf9b3227005ba9b6
mark200462 <144628713+mark200462@users.noreply.github.com>
2024-04-21 17:46:31 +0200

    Delete backend directory

--- backend/routes/router.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-
-const router: Router = express.Router();
-
-router.use('/users', require("./Users/Users")());
-router.use('/task', require("./Task/Task")());
-router.use('/notification', require("./Notification/Notification")());
-
-module.exports = router;
